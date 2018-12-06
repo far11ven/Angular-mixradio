5bacdc6300e0aeef3708956554c9ded9b6a81b57
far11ven <kushalbhalaik@gmail.com>
2018-12-06 13:56:57 +0530

    Changed to run on Heroku
    Changed to run on Heroku

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/build'));
 app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
 
-app.get('/*', function(req,res){
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
 
 console.log('Server started running..');
