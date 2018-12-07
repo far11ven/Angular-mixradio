bd3d2a8a5166d3837b90ebdfe3e1269fdf71eefa
far11ven <kushalbhalaik@gmail.com>
2018-12-07 16:00:51 +0530

    Changed to run on Heroku
    Changed to run on Heroku

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.listen(process.env.PORT || 8080);
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/index.html');
+  const fullPath = path.join(__dirname + '/dist/music-app/index.html');
   console.log(" Fetching from.." + fullPath);
 	res.sendFile(fullPath);
 })
 
-console.log('Server started running..');
+console.log('Server started running..' + process.env.PORT || 8080);
 
 //Changed to run on Heroku
